e24d68603247db0f9c14423ffd4374b83c7a92fa
Bryan Soong <whiterevolver35@gmail.com>
2021-04-26 21:46:36 +0800

    :fire: wrap up:

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,18 +1,19 @@
 import './hero.css';
 import TextLoop from "react-text-loop";
 import Earth from '../assets/earth.svg';
+import Sun from '../assets/sun.svg';
 
 export default function Hero() {
     
-    const languages = ["Hello", "您好"]
+    const languages = ["Hello", "您好", "Bonjour", "Annyeong", "Hola", "Ciao"];
 
     return (
         <div className="hero-container">
             <div className="hero-intro-container">
                 <h2 className="hero-intro-title"><TextLoop children={languages}></TextLoop>.</h2>
-                <p className="hero-intro-text">I am Bryan Soong, a self-taught passionate hobbyist programmer. I like to innovate and create new things! I hope to one day change the world for the better.</p>
+                <p className="hero-intro-text">I am Bryan Soong, a self-taught but passionate hobbyist programmer. I like to innovate and create new things!</p>
             </div>
             <img src={Earth} className="hero-earth" alt="earth"></img>
         </div>
-    )
-}
+    );
+};
