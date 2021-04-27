6553807961912ed87e77eab0cee4ee17a03fed33
Bryan Soong <whiterevolver35@gmail.com>
2021-04-27 23:08:25 +0800

    :sparkles: removed sparkle animation

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,11 +1,13 @@
 import Header from '../components/header';
 import Hero from '../components/hero';
+import Section from '../components/section';
 
 export default function Home() {
   return (
     <div className="Home">
       <Header />
       <Hero />
+      <Section />
     </div>
   );
 }
--- src/components/section.js
+++ src/components/section.js
@@ -1,3 +1,9 @@
+import './section.css';
+
 export default function Section() {
-    
+    return (
+        <div className="section-container">
+
+        </div>
+    )
 }
