219c0339b0100c46a9411090d048194b3c61fbcd
Bryan Soong <whiterevolver35@gmail.com>
2021-04-28 00:24:35 +0800

    :sparkles: refactored section.js

--- src/components/section.js
+++ src/components/section.js
@@ -1,15 +1,15 @@
 import './section.css';
 import ProfilePicture from '../assets/profile.jpg';
 
-export default function Section() {
+export default function Section(props) {
     return (
         <div className="section-container">
             <div className="image-container">
-                <img src={ProfilePicture} className="section-image" alt="Profile"></img>
+                <img src={props.image} className="section-image" alt="section-1"></img>
             </div>
             <div className="text-container">
-                <h2 className="section-title">About Me</h2>
-                <p className="section-text">Placeholder text is the label for possible content in a text box. It can normally be found when there are prompts to fill out a form. It’s the hint that tells you ‘Last name’ or the format with which to enter your birthdate or phone number. Placeholder text typically exists as a hint to fill in actual text.</p>
+                <h2 className="section-title">{props.title}</h2>
+                <p className="section-text">{props.text}</p>
             </div>
         </div>
     )
