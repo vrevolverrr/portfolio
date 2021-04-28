102c929b46637445a3cf60e10b3d753c52024f4d
Bryan Soong <whiterevolver35@gmail.com>
2021-04-28 14:38:08 +0800

    :sparkles: added footer

--- src/components/section.js
+++ src/components/section.js
@@ -1,15 +1,27 @@
 import './section.css';
 
 export default function Section(props) {
+    const shouldInvertHorizontally = props.invert || false;
+
     return (
         <div className="section-container">
-            <div className="image-container">
-                <img src={props.image} className="section-image" alt="section-1"></img>
-            </div>
-            <div className="text-container">
-                <h2 className="section-title">{props.title}</h2>
-                <p className="section-text">{props.text}</p>
-            </div>
+            {(shouldInvertHorizontally) ?
+                [<div className="text-container">
+                    <h2 className="section-title">{props.title}</h2>
+                    <p className="section-text">{props.text}</p>
+                </div>,
+                <div className="image-container">
+                    <img src={props.image} className="section-image" alt="section-1"></img>
+                </div>]
+            :
+                [<div className="image-container">
+                    <img src={props.image} className="section-image" alt="section-1"></img>
+                </div>,
+                <div className="text-container">
+                    <h2 className="section-title">{props.title}</h2>
+                    <p className="section-text">{props.text}</p>
+                </div>]
+            }
         </div>
     )
 }
