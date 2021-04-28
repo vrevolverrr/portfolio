278e5f7aae9fbe855d1c40f0621380aab1e4f3a4
Bryan Soong <whiterevolver35@gmail.com>
2021-04-28 14:38:53 +0800

    :sparkles: updated section title

--- src/app/Home.js
+++ src/app/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Hero />
       <Section
         image={ProfilePicture}
-        title="About"
+        title="About Me"
         text="Placeholder text is the label for possible content in a text box. It can normally be found when there are prompts to fill out a form. It’s the hint that tells you ‘Last name’ or the format with which to enter your birthdate or phone number. Placeholder text typically exists as a hint to fill in actual text."/>
       <Toast />
       <Footer />
