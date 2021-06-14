9eb5c0e1e5bdfe2dce3cf6303001e6244ca8e321
Bryan Soong <whiterevolver35@gmail.com>
2021-06-14 15:57:53 +0800

    :sparkles: added new project

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ export default function Header() {
             <a href="https://github.com/vrevolverrr/portfolio" className="wip-signboard"><img src={Signboard} alt="wip-signboard"></img></a>
             <a href="/" className="page-title"><img src={Title} alt="title"></img></a>
             <p onClick={() => showToast("I'm sorry but that's not implemented yet")} className="header-nav-item">RESUME</p>
-            <p onClick={() => showToast("I'm sorry but that's not implemented yet")} className="header-nav-item">PROJECTS</p>
+            <p onClick={() => document.querySelector(".projects-container").scrollIntoView({behavior: 'smooth', block: 'end'})} className="header-nav-item">PROJECTS</p>
             <p onClick={() => showToast("I'm sorry but that's not implemented yet")} className="header-nav-item" style={{marginRight: "20px"}}>CONTACT</p>
         </div>
     )
